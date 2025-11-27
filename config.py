4be1de45a032e99bf32fb0d6d6456d69dd06c1c5
a2solutionserviece-glitch <a2.solutionserviece@gmail.com>
2025-11-27 19:19:03 +0530

    New File Editaed

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key")
-    DATABASE_URL = os.getenv("DATABASE_URL")
+    DATABASE_URL = "postgresql://admin:2hFmivuYCP5n8A9V71xMfrOYWxE8sfOT@dpg-d4k4ob0gjchc739uboq0-a/hotelym"
 
 class DevConfig(Config):
     DEBUG = True
